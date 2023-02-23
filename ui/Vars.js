fe6f505c851843fb0fadb6c5a31e0bf3ea3dc497
Vaclav Siml <kryvas199@gmail.com>
2023-02-23 23:58:44 +0100

    spawn

--- ui/Vars.js
+++ ui/Vars.js
@@ -1,5 +1,5 @@
 var AsphyxiaPath = "C:\\Users\\kryvas\\Downloads\\asphyxia-core-win-x64\\asphyxia.exe"
-var SDVX3Path = "C:\\Users\\kryvas\\Downloads\\sdvx\\Sound voltex iii gravity wars\\spice.exe"
+var SDVX3Path = "C:\\UI\\arcade-machine\\ui\\spice.exe"
 var SDVX4Path = ""
 var SDVX5Path = ""
 var browser = "brave.exe"
