9412091524e1e58fe1abaa75e57ea9bc66815978
Vaclav Siml <kryvas199@gmail.com>
2023-02-24 20:34:12 +0100

    Added configurable menu keys part 2

--- ui/Vars.js
+++ ui/Vars.js
@@ -1,5 +1,14 @@
+//Paths
 var AsphyxiaPath = "C:\\Users\\kryvas\\Downloads\\asphyxia-core-win-x64\\asphyxia.exe"
-var SDVX3Path = "C:\\UI\\arcade-machine\\ui\\spice.exe"
+var SDVX3Path = "C:\\sdvx\\spice2.lnk"
 var SDVX4Path = ""
 var SDVX5Path = ""
 var browser = "brave.exe"
+//Keys
+var KeyUp = "KeyW"
+var KeyDown = "KeyS"
+var KeyLeft = "KeyA"
+var KeyRight = "KeyD"
+var KeyStart = "KeyE"
+var ExitKey1 = "g"       // Only add lowercase name of the key without "Key" part  example: "KeyA" becomes "a"
+var ExitKey2 = "h"       // Only add lowercase name of the key without "Key" part  example: "KeyA" becomes "a"
