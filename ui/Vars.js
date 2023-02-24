3e4a7335ea321ee159a3c5e0b58b49c918d2b8b0
Vaclav Siml <kryvas199@gmail.com>
2023-02-25 00:00:20 +0100

    Can now kill game exe with globalshortcut

--- ui/Vars.js
+++ ui/Vars.js
@@ -1,3 +1,4 @@
+
 //Paths
 var AsphyxiaPath = "C:\\Users\\kryvas\\Downloads\\asphyxia-core-win-x64\\asphyxia.exe"
 var SDVX3Path = "C:\\sdvx\\spice2.lnk"
@@ -10,5 +11,8 @@ var KeyDown = "KeyS"
 var KeyLeft = "KeyA"
 var KeyRight = "KeyD"
 var KeyStart = "KeyE"
-var ExitKey1 = "g"       // Only add lowercase name of the key without "Key" part  example: "KeyA" becomes "a"
-var ExitKey2 = "h"       // Only add lowercase name of the key without "Key" part  example: "KeyA" becomes "a"
+var ExitKey1 = "G"       // Only add uppercase name of the key without "Key" part  example: "KeyA" becomes "A"
+var ExitKey2 = "H"       // Only add uppercase name of the key without "Key" part  example: "KeyA" becomes "A"
+    
+if(module) // kdyz to neimportujes pres require tak module bude undefined
+module.exports = { ExitKey1, ExitKey2 };
--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,12 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const { globalShortcut } = require('electron');
 const createWindow = () => {
-    // Create the browser window.
+  ipcMain.on("number",(event,data)=>{ //Reads number var from index
+    console.warn(data)
+    Execounter = data;
+    console.warn(Execounter)
+  })  
+  // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -12,12 +17,39 @@ const createWindow = () => {
         },
         autoHideMenuBar: true
     });
-    mainWindow.loadFile("index.html") //loadne index.html
+          mainWindow.loadFile("index.html") //loads index.html
+          
+
+         
+    
 };
 
 
 app.whenReady().then(() => {
-    createWindow();
+  createWindow(); 
+  
+  const { ExitKey1, ExitKey2 } = require("./Vars.js") //imports exitkeys from vars.js
+  const { execFile, exec, spawn } = require('child_process');
+  console.log(ExitKey1 + " + " + ExitKey2);
+  
   const ret = globalShortcut.register(ExitKey1 + " + " + ExitKey2, () => { //makes globalshortcut from vars in vars.js
     console.log('CommandOrControl+X is pressed')
-});    
+    //On shortcut kills game exe
+    switch(Execounter){
+      case 1:
+        console.warn("funguje" + Execounter);
+        exec("taskkill /im spice.exe")
+        break;
+      case 2:
+        console.warn("funguje" + Execounter);
+        exec("taskkill /im spice.exe")
+        break;
+      case 3:
+        console.warn("funguje" + Execounter);
+        exec("taskkill /im spice.exe")
+        break;
+      default:
+        break;
+    }
+  })
+  });    
